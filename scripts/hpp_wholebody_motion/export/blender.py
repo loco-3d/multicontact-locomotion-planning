c1b8f2da7f9b2e480c7c1aa173661777bb501719
pFernbach <pierre.fernbach@gmail.com>
2019-02-07 17:11:13 +0100

    [export] add a method to export the motion to blender

---
+++ scripts/hpp_wholebody_motion/export/blender.py
@@ -0,0 +1,17 @@
+import os
+import hpp_wholebody_motion.config as cfg
+import hpp_wholebody_motion.viewer.display_tools as display_tools
+
+def export(q_t,v):
+    path = cfg.EXPORT_PATH+"/blender"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    filename = path+"/"+cfg.DEMO_NAME+".yaml"
+    nodes = [cfg.Robot.urdfName]
+    v.client.gui.setCaptureTransform(filename,nodes)
+    display_tools.displayWBconfig(v,q_t[0])
+    v.client.gui.captureTransformOnRefresh(True)
+    display_tools.displayWBmotion(v,q_t,cfg.IK_dt,cfg.DT_DISPLAY)
+    v.client.gui.captureTransformOnRefresh(False)
+    
+    
