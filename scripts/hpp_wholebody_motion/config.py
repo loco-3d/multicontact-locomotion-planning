a37f565e9ff6abf2300896319b9901148639ea6c
pFernbach <pierre.fernbach@gmail.com>
2019-03-15 12:21:04 +0100

    [config] improve error message about python argument and display all available choices

--- scripts/hpp_wholebody_motion/config.py
+++ scripts/hpp_wholebody_motion/config.py
@@ -5,8 +5,8 @@ PKG_PATH = os.environ['DEVEL_HPP_DIR']+"/src/hpp-wholebody-motion"
 OUTPUT_DIR = PKG_PATH+"/res"
 CONTACT_SEQUENCE_PATH = OUTPUT_DIR + "/contact_sequences"
 TIME_OPT_CONFIG_PATH = PKG_PATH +'/timeOpt_configs'
-LOAD_CS = True
-LOAD_CS_COM = True
+LOAD_CS = False
+LOAD_CS_COM = False
 SAVE_CS = not LOAD_CS and True 
 SAVE_CS_COM = not LOAD_CS_COM and True
 EXPORT_GAZEBO = True
@@ -16,7 +16,7 @@ EXPORT_PATH = OUTPUT_DIR+"/export"
 DISPLAY_CS = False # display contact sequence from rbprm
 DISPLAY_CS_STONES = True # display stepping stones
 DISPLAY_INIT_GUESS_TRAJ = False 
-DISPLAY_WP_COST=False
+DISPLAY_WP_COST=True
 DISPLAY_COM_TRAJ = True
 DISPLAY_FEET_TRAJ = True
 DISPLAY_WB_MOTION = True
@@ -41,7 +41,7 @@ EFF_CHECK_COLLISION = True
 ##  Settings for whole body : 
 YAW_ROT_GAIN = 1.
 USE_CROC_COM = False
-WB_VERBOSE = False
+WB_VERBOSE = True
 WB_STOP_AT_EACH_PHASE = False
 IK_dt = 0.001  # controler time step
 IK_PRINT_N = 500  # print state of the problem every IK_PRINT_N time steps (if verbose = True)
@@ -51,6 +51,13 @@ IK_PRINT_N = 500  # print state of the problem every IK_PRINT_N time steps (if v
 import importlib
 import sys
 if len(sys.argv)<2 : 
+    print "You must call this script with the name of the config file (one of the file contained in hpp_wholebody_motion.demo_config)"
+    print "Available demo files : "
+    configs_path = PKG_PATH+"/scripts/hpp_wholebody_motion/demo_configs"
+    demos_list = os.listdir(configs_path)
+    for f in demos_list:
+        if f.endswith(".py") and not f.startswith("__") : 
+            print f.rstrip(".py")
     raise IOError("You must call this script with the name of the config file (one of the file contained in hpp_wholebody_motion.demo_config)")
     
 import argparse
