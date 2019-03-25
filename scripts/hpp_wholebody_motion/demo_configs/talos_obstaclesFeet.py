f95552ff222b6c2c278e9a774ac2a7f61af90818
pFernbach <pierre.fernbach@gmail.com>
2019-03-25 14:02:24 +0100

    [configs] add files for anymal

--- scripts/hpp_wholebody_motion/demo_configs/talos_obstaclesFeet.py
+++ scripts/hpp_wholebody_motion/demo_configs/talos_obstaclesFeet.py
@@ -5,7 +5,7 @@ SCRIPT_PATH = "demos"
 DURATION_INIT = 1.5 # Time to init the motion
 DURATION_FINAL = 1.5 # Time to stop the robot
 DURATION_FINAL_SS = 1.
-DURATION_SS =1.4
+DURATION_SS =2.0
 DURATION_DS = 0.3
 DURATION_TS = 0.4
 
