f752702b6648e22d66db8c14a51501879bd7a5e5
pFernbach <pierre.fernbach@gmail.com>
2019-04-10 16:05:02 +0200

    [config] add configs for AM task

--- scripts/hpp_wholebody_motion/demo_configs/common_hrp2.py
+++ scripts/hpp_wholebody_motion/demo_configs/common_hrp2.py
@@ -1,21 +1,29 @@
 from hpp.corbaserver.rbprm.hrp2 import Robot
+## following info are already accessible once the urdf is loaded, but here we do not load the urdf ...
 MASS = 55.88363633
-
+nq = 37
+nv = 36
 ## weight and gains used by TSID
 
 fMin = 1.0                      # minimum normal force
-fMax = 1000.0                   # maximum normal force
-w_com = 1.0                     # weight of center of mass task
-w_posture = 0.001                 # weight of joint posture task
-w_rootOrientation = 1.0         # weight of the root's orientation task
+fMax = 800.                   # maximum normal force
+w_com = 1.0           # weight of center of mass task
+w_am = 1.
+w_posture = 0.1                 # weight of joint posture task
+w_rootOrientation = 1.       # weight of the root's orientation task
 w_forceRef = 1e-3               # weight of force regularization task
 w_eff = 1.0                     # weight of the effector motion task
-kp_contact = 30.0               # proportional gain of contact constraint
-kp_com = 1000.0                 # proportional gain of center of mass task
-kp_posture = 50.                # proportional gain of joint posture task
-kp_rootOrientation = 1000.     # proportional gain of the root's orientation task
-kp_Eff = 1000.                   # proportional gain ofthe effectors motion task
-
+kp_contact = 300.0               # proportional gain of contact constraint
+kp_com = 100.                 # proportional gain of center of mass task
+kp_am = 100.
+kp_posture = 1.              # proportional gain of joint posture task
+kp_rootOrientation = 500.     # proportional gain of the root's orientation task
+kp_Eff = 300.                   # proportional gain ofthe effectors motion task
+level_eff = 0
+level_com = 0
+level_posture = 1
+level_rootOrientation = 1
+level_am = 1
 
 IK_dt = 0.005
 
