298ab84c1e46ffa5c3f0233788d9ffc7fa62764a
pFernbach <pierre.fernbach@gmail.com>
2019-04-28 23:19:53 +0200

    [plot] do not crash when an effector doesn't move for all the motion

--- scripts/mlp/utils/plot.py
+++ scripts/mlp/utils/plot.py
@@ -30,8 +30,9 @@ def plotEffectorRef(dict_refs,dt):
     # look for the maximal time in the trajectories :
     t_max = 0.
     for trajs in dict_refs.itervalues():
-        if trajs[-1].time_interval[-1] > t_max:
-            t_max = trajs[-1].time_interval[-1]
+        if len(trajs) > 0:
+            if trajs[-1].time_interval[-1] > t_max:
+                t_max = trajs[-1].time_interval[-1]
     N = int(t_max/dt) +1
     timeline =  np.matrix([i*dt for i in range(N)])
     #print "plot : tmax = ",t_max
@@ -40,40 +41,41 @@ def plotEffectorRef(dict_refs,dt):
     #print "len timeline : ",timeline.shape
     # One plot for each effector present in the dict :
     for eeName,trajs in dict_refs.iteritems():
-        # first build matrices of position, velocity and acceleration (each is 3D)  :
-        values = np.matrix(np.zeros([9,N]))
-        id_traj = 0
-        traj = trajs[id_traj]
-        # save current pos (or next) and use it when timing not inside bounds
-        pos =  traj.placement_init.translation
-        for i,t in np.ndenumerate(timeline) : 
-            i=i[1]
-            #print "t =",t
-            if t > traj.time_interval[-1] and id_traj < (len(trajs)-1):  # take next traj in list
-                pos =  traj.placement_end.translation
-                id_traj += 1
-                traj = trajs[id_traj]
-                #print "new traj, t0 = ",traj.time_interval[0]
-            if t >= traj.time_interval[0] and t <= traj.time_interval[-1]:
-                values[0:3,i] = traj.curves(t-traj.time_interval[0])
-                values[3:6,i] = traj.curves.d(t-traj.time_interval[0])
-                values[6:9,i] = traj.curves.dd(t-traj.time_interval[0])
-                #print "add column ",i
-                #print " values : ",values[:,i]
-            else :
-                values[0:3,i] =pos
-        #print "values shape : ",values.shape
-        # then plot each line of values in a graph  :
-        fig, ax = plt.subplots(3,3)
-        fig.canvas.set_window_title("Reference trajectory effector "+eeName)
-        fig.suptitle("Reference trajectory effector "+eeName, fontsize=20)
-        for i in range(3): # line = pos,vel,acc
-            for j in range(3): # col = x,y,z
-                ax_sub = ax[i,j]
-                ax_sub.plot(timeline.T, values[i*3 + j,:].T, color=colors[j])
-                ax_sub.set_xlabel('time (s)')
-                ax_sub.set_ylabel(labels[i*3 + j])
-                ax_sub.grid(True)
+        if len(trajs) > 0 :
+            # first build matrices of position, velocity and acceleration (each is 3D)  :
+            values = np.matrix(np.zeros([9,N]))
+            id_traj = 0
+            traj = trajs[id_traj]
+            # save current pos (or next) and use it when timing not inside bounds
+            pos =  traj.placement_init.translation
+            for i,t in np.ndenumerate(timeline) : 
+                i=i[1]
+                #print "t =",t
+                if t > traj.time_interval[-1] and id_traj < (len(trajs)-1):  # take next traj in list
+                    pos =  traj.placement_end.translation
+                    id_traj += 1
+                    traj = trajs[id_traj]
+                    #print "new traj, t0 = ",traj.time_interval[0]
+                if t >= traj.time_interval[0] and t <= traj.time_interval[-1]:
+                    values[0:3,i] = traj.curves(t-traj.time_interval[0])
+                    values[3:6,i] = traj.curves.d(t-traj.time_interval[0])
+                    values[6:9,i] = traj.curves.dd(t-traj.time_interval[0])
+                    #print "add column ",i
+                    #print " values : ",values[:,i]
+                else :
+                    values[0:3,i] =pos
+            #print "values shape : ",values.shape
+            # then plot each line of values in a graph  :
+            fig, ax = plt.subplots(3,3)
+            fig.canvas.set_window_title("Reference trajectory effector "+eeName)
+            fig.suptitle("Reference trajectory effector "+eeName, fontsize=20)
+            for i in range(3): # line = pos,vel,acc
+                for j in range(3): # col = x,y,z
+                    ax_sub = ax[i,j]
+                    ax_sub.plot(timeline.T, values[i*3 + j,:].T, color=colors[j])
+                    ax_sub.set_xlabel('time (s)')
+                    ax_sub.set_ylabel(labels[i*3 + j])
+                    ax_sub.grid(True)
 
 def plotEffectorTraj(timeline,p_intervals,ref_dict,traj_dict):
     labels=["x (m)" , "y (m)" ,"z (m)"]
