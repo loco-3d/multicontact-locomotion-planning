4928e5169a730beaf005d146728a5d357fbb33d0
pFernbach <pierre.fernbach@gmail.com>
2019-04-29 22:36:16 +0200

    [config] add demo_config for talos_moveEffector_stairs

---
+++ scripts/mlp/demo_configs/talos_moveEffector_stairs_m15.py
@@ -0,0 +1,20 @@
+TIMEOPT_CONFIG_FILE = "cfg_softConstraints_talos_eff9.yaml"
+from common_talos import *
+SCRIPT_PATH = "memmo"
+#ENV_NAME = "multicontact/ground"
+
+kp_am = 1.
+w_am = 0.5
+
+DURATION_INIT = 2. # Time to init the motion
+DURATION_FINAL = 1.5 # Time to stop the robot
+DURATION_CONNECT_GOAL = 0.
+DURATION_SS =1.6
+
+
+EFF_T_PREDEF = 0.3
+EFF_T_DELAY = 0.05
+p_max = 0.12
+
+YAW_ROT_GAIN = 1e-3
+end_effector_method = "limbRRToptimized" 
