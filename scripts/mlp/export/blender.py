87191c4dc0e564a6f8cf217c75c9f56dfad3b299
pFernbach <pierre.fernbach@gmail.com>
2019-05-01 15:45:01 +0200

    [export] fix mistakes in blender export

--- scripts/mlp/export/blender.py
+++ scripts/mlp/export/blender.py
@@ -17,11 +17,11 @@ def export(q_t,v,dt):
     assert step%1 == 0 ,"display dt should be a multiple of dt"
     step = int(step)
     while id < q_t.shape[1]:
-        displayWBconfig(viewer,q_t[:,id])
+        display_tools.displayWBconfig(v,q_t[:,id])
         v.client.gui.captureTransform()
         id += step
     # display last config if the total duration is not a multiple of the dt
-    displayWBconfig(viewer,q_t[:,-1])
+    display_tools.displayWBconfig(v,q_t[:,-1])
     v.client.gui.captureTransform()    
     print "motion exported to ",filename
     
