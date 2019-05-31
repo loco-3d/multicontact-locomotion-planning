49b4517737d1924dd89e2725064add90082b4cab
pFernbach <pierre.fernbach@gmail.com>
2019-05-31 19:42:29 +0200

    [configs] working configurations for ANYmal

--- scripts/mlp/demo_configs/common_anymal.py
+++ scripts/mlp/demo_configs/common_anymal.py
@@ -1,23 +1,23 @@
-from hpp.corbaserver.rbprm.anymal_contact6D import Robot
+from hpp.corbaserver.rbprm.anymal import Robot
 MASS = 30.47 # cannot retrieve it from urdf because the file is not parsed here ... 
 
 ## weight and gains used by TSID
 
 fMin = 1.0                      # minimum normal force
-fMax = 200.                  # maximum normal force
-w_com = 1.0                     # weight of center of mass task
-w_posture = 1e-3                 # weight of joint posture task
-w_am = 1.
-w_rootOrientation = 1.0         # weight of the root's orientation task
-w_forceRef = 1e-5               # weight of force regularization task
+fMax = 1000.                  # maximum normal force
+w_com = 1.0           # weight of center of mass task
+w_am = 0.
+w_posture = 0                # weight of joint posture task
+w_rootOrientation = 1       # weight of the root's orientation task
+w_forceRef = 1e-3               # weight of force regularization task
 w_eff = 1.0                     # weight of the effector motion task
-kp_contact = 10.0               # proportional gain of contact constraint
-kp_com = 10.0                # proportional gain of center of mass task
-kp_posture = 10.0               # proportional gain of joint posture task
-kp_am = 10.
-kp_rootOrientation = 50.0     # proportional gain of the root's orientation task
-kp_Eff = 100.0                 # proportional gain ofthe effectors motion task
-level_eff = 0
+kp_contact = 30.0               # proportional gain of contact constraint
+kp_com = 20.                 # proportional gain of center of mass task
+kp_am = 20.
+kp_posture = 50.              # proportional gain of joint posture task
+kp_rootOrientation = 1000.     # proportional gain of the root's orientation task
+kp_Eff = 50000.                   # proportional gain ofthe effectors motion task
+level_eff = 1
 level_com = 0
 level_posture = 1
 level_rootOrientation = 1
@@ -25,12 +25,24 @@ level_am = 1
 
 IK_eff_size = Robot.dict_size.copy()
 
+# phases duration
+DURATION_INIT = 1. # Time to init the motion
+DURATION_FINAL = 1. # Time to stop the robot
+DURATION_FINAL_SS = 1.
+DURATION_SS =1.2
+DURATION_DS = 1.2
+DURATION_TS = 0.6
+DURATION_QS = 0.6
+DURATION_CONNECT_GOAL = 1.
+
 ## Settings for end effectors : 
-EFF_T_PREDEF = 0.1
-EFF_T_DELAY = 0.05
-p_max = 0.1
+EFF_T_PREDEF = 0.
+p_max = 0.2
+EFF_T_DELAY = 0.
+FEET_MAX_VEL = 1.
+FEET_MAX_ANG_VEL = 1.5
 
 import numpy as np
-gain_vector = np.matrix(np.ones(24)).T
-masks_posture = np.matrix(np.zeros(24)).T
+gain_vector = np.matrix(np.ones(12)).T
+masks_posture = np.matrix(np.zeros(12)).T
 
