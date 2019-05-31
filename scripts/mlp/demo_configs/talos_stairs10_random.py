98b315a2bf20ac133266acd15559808abe6d14f4
pFernbach <pierre.fernbach@gmail.com>
2019-05-31 19:43:36 +0200

    [config] add demo_config for talos in stairs 10cm

---
+++ scripts/mlp/demo_configs/talos_stairs10_random.py
@@ -0,0 +1,16 @@
+TIMEOPT_CONFIG_FILE = "cfg_softConstraints_talos.yaml"
+from common_talos import *
+SCRIPT_PATH = "memmo"
+ENV_NAME = "multicontact/bauzil_stairs"
+
+DURATION_INIT = 1.5 # Time to init the motion
+DURATION_FINAL = 1 # Time to stop the robot
+DURATION_FINAL_SS = 1.
+DURATION_SS =1.6
+DURATION_DS = 0.3
+DURATION_TS = 0.4
+DURATION_CONNECT_GOAL = 1.
+
+
+#end_effector_method = "bezierPredef" 
+#EFF_CHECK_COLLISION = False
