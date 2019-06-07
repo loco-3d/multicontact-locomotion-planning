3ce74972243cb2373a3246139ea10fabc9985ff3
pFernbach <pierre.fernbach@gmail.com>
2019-06-07 13:11:11 +0200

    [cs] add manually defined example of walking on flat ground

---
+++ scripts/mlp/contact_sequence/manually_defined/walk_1m.py
@@ -0,0 +1,23 @@
+from multicontact_api import ContactSequenceHumanoid, ContactPhaseHumanoid
+import mlp.viewer.display_tools as display_tools
+from mlp.utils.cs_tools import *
+import mlp.config as cfg
+
+
+from hpp.corbaserver.rbprm.talos import Robot # change robot here
+ENV_NAME = "multicontact/ground"
+
+fb,v = display_tools.initScene(Robot,ENV_NAME)
+cs = ContactSequenceHumanoid(0)
+
+
+#Create an initial contact phase : 
+q_ref = fb.referenceConfig[::] + [0]*6
+addPhaseFromConfig(fb,v,cs,q_ref,[fb.rLegId,fb.lLegId])
+
+walk(Robot,v,cs,1.2,0.4,[fb.rLegId,fb.lLegId])
+
+DEMO_NAME = "talos_flatGround"
+filename = cfg.CONTACT_SEQUENCE_PATH + "/"+DEMO_NAME+".cs"
+print "Write contact sequence binary file : ",filename
+cs.saveAsBinary(filename) 
---
+++ scripts/mlp/contact_sequence/manually_defined/walk_oneStep30.py
@@ -0,0 +1,31 @@
+from multicontact_api import ContactSequenceHumanoid, ContactPhaseHumanoid
+import mlp.viewer.display_tools as display_tools
+from mlp.utils.cs_tools import *
+import mlp.config as cfg
+
+
+from hpp.corbaserver.rbprm.talos import Robot # change robot here
+ENV_NAME = "multicontact/ground"
+
+fb,v = display_tools.initScene(Robot,ENV_NAME)
+cs = ContactSequenceHumanoid(0)
+
+
+
+
+#Create an initial contact phase : 
+q_ref = fb.referenceConfig[::] + [0]*6
+addPhaseFromConfig(fb,v,cs,q_ref,[fb.rLegId,fb.lLegId])
+
+moveEffectorOf(Robot,v,cs,fb.rfoot,[0.3,0,0])
+moveEffectorOf(Robot,v,cs,fb.lfoot,[0.3,0,0])
+
+q_end = q_ref[::]
+q_end[0] += 0.3
+setFinalState(cs,q=q_end)
+
+
+DEMO_NAME = "talos_flatGround"
+filename = cfg.CONTACT_SEQUENCE_PATH + "/"+DEMO_NAME+".cs"
+print "Write contact sequence binary file : ",filename
+cs.saveAsBinary(filename) 
