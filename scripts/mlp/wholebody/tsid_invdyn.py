904dc10511362484246d43e17ece6fcf86e79468
pFernbach <pierre.fernbach@gmail.com>
2019-06-24 13:52:30 +0200

    [wb] tsid : clean unused code

--- scripts/mlp/wholebody/tsid_invdyn.py
+++ scripts/mlp/wholebody/tsid_invdyn.py
@@ -44,8 +44,6 @@ def getCurrentEffectorPosition(robot,data,eeName):
             
     
 def createContactForEffector(invdyn,robot,phase,eeName):             
-    z_up = np.matrix([0., 0., 1.]).T  
-    #contactNormal = getContactPlacement(phase,eeName).rotation * z_up # direction of the normal to the contact surface
     contactNormal = np.matrix(cfg.Robot.dict_normal[eeName]).T
     contactNormal = cfg.Robot.dict_offset[eeName].rotation * contactNormal # apply offset transform
     if cfg.Robot.cType == "_3_DOF":
