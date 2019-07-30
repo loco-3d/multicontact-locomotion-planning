66b29a3cb0bc14c8164f2bbb75854183fe77c592
Fernbach Pierre <pFernbach@users.noreply.github.com>
2019-07-30 22:42:11 +0200

    [config] common_talos : add comments

--- scripts/mlp/demo_configs/common_talos.py
+++ scripts/mlp/demo_configs/common_talos.py
@@ -4,11 +4,13 @@ MASS = 90.27
 ## predef duration of contact phases : 
 DURATION_INIT = 1. # Time to init the motion
 DURATION_FINAL = 1.5 # Time to stop the robot
-DURATION_FINAL_SS = 1.
-DURATION_SS =1.
-DURATION_DS = 0.2
-DURATION_TS = 0.4
-DURATION_CONNECT_GOAL = 2.
+DURATION_FINAL_SS = 1. # duration of the final phase if it's a single support phase
+DURATION_SS =1. # duration of the single support phases
+DURATION_DS = 0.2 # duration of the double support phases
+DURATION_TS = 0.4 # duration of the triple support phases
+DURATION_CONNECT_GOAL = 2. # duration to try to connect the last points in the CoM trajectory with the goal position given to planning
+# Hardcoded height change of the COM before the beginning of the motion (value in m and time allowed to make this motion)
+# This is used because for some robot, the reference configuration is really close to the kinematic limits of the robot. 
 COM_SHIFT_Z = -0.015
 TIME_SHIFT_COM = 1.5
 
@@ -38,23 +40,23 @@ IK_eff_size = Robot.dict_size.copy()
 #IK_eff_size={Robot.rfoot:[0.1 , 0.06], Robot.lfoot:[0.1 , 0.06],Robot.rhand:[0.1, 0.1],Robot.lhand:[0.1, 0.1]}
 
 ## Settings for end effectors : 
-EFF_T_PREDEF = 0.2
-EFF_T_DELAY = 0.05
-FEET_MAX_VEL = 0.5
-FEET_MAX_ANG_VEL = 1.5
-p_max = 0.07
+EFF_T_PREDEF = 0.2 # duration during which the motion of the end effector is forced to be orthogonal to the contact surface, at the beginning and the end of the phase
+EFF_T_DELAY = 0.05 # duration at the beginning and the end of the phase where the effector don't move
+FEET_MAX_VEL = 0.5 # maximal linear velocity of the effector, if the current duration of the phase lead to a greater velocity than this setting, the duration of the phase is increased
+FEET_MAX_ANG_VEL = 1.5 # maximal angular velocity of the effectors
+p_max = 0.07 #setting used to compute the default height of the effector trajectory. end_effector/bezier_predef.py : computePosOffset()
 
 import numpy as np
-gain_vector = np.matrix(
+gain_vector = np.matrix(  # gain vector for postural task
     [ 10. ,  5.  , 5. , 1. ,  1. ,  10., # lleg  #low gain on axis along y and knee
     10. ,  5.  , 5. , 1. ,  1. ,  10., #rleg
     500. , 500.  , #chest
     50.,   100.  , 10.,  10.,    10. ,  10. , 100. ,  50. , #larm
     50.,   100.  , 10., 10.,    10. ,  10. ,  100. ,  50. , #rarm
    100.,  100.] #head
-    ).T   # gain vector for postural task :
+    ).T  
 
 masks_posture = np.matrix(np.ones(32)).T
 #masks_posture[:11] = 0
 
-IK_REFERENCE_CONFIG = np.matrix(Robot.referenceConfig).T
+IK_REFERENCE_CONFIG = np.matrix(Robot.referenceConfig).T #Reference config used by the wholeBody script, may be different than the one used by the planning (default value is the same as planning)
