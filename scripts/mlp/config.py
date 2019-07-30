8b5dc9ef41af4d5b09bee4652b9bb4d6abe6a15b
Fernbach Pierre <pFernbach@users.noreply.github.com>
2019-07-30 22:31:42 +0200

    [config] clean and add comment

--- scripts/mlp/config.py
+++ scripts/mlp/config.py
@@ -20,60 +20,63 @@ OUTPUT_DIR = PKG_PATH+"/res"
 CONTACT_SEQUENCE_PATH = OUTPUT_DIR + "/contact_sequences"
 TIME_OPT_CONFIG_PATH = PKG_PATH +'/timeOpt_configs'
 STATUS_FILENAME = OUTPUT_DIR + "/infos.log"
+EXPORT_PATH = OUTPUT_DIR+"/export"
+## Export setting
 SAVE_CS = True 
 SAVE_CS_COM = True
 EXPORT_GAZEBO = False
-EXPORT_OPENHRP = False
 EXPORT_NPZ = True
 EXPORT_BLENDER = False
 EXPORT_SOT = False
-openHRP_useZMPref = False
-EXPORT_PATH = OUTPUT_DIR+"/export"
+EXPORT_OPENHRP = False
+openHRP_useZMPref = False # if true : in the export_openHRP, use the zmp computed by the centroidal solver and the one computed from the wholebody
 WRITE_STATUS = True
 ##DISPLAY settings : 
 DISPLAY_CS = False # display contact sequence from rbprm
 DISPLAY_CS_STONES = True # display stepping stones
 DISPLAY_INIT_GUESS_TRAJ = False 
-DISPLAY_WP_COST=True
-DISPLAY_COM_TRAJ = True
+DISPLAY_WP_COST=True # display waypoints found by the planner and used in the cost function of the centroidal dynamic solver
+DISPLAY_COM_TRAJ = True 
 DISPLAY_FEET_TRAJ = True # display the feet trajectories used in the final motion
 DISPLAY_ALL_FEET_TRAJ = False # display all the trajectory used as reference, even the invalid ones
-DISPLAY_WB_MOTION = False
-DT_DISPLAY = 0.05 # dt used to display the wb motion
-PLOT = False
-DISPLAY_PLOT = PLOT and True
-SAVE_PLOT = PLOT and True
-PLOT_CENTROIDAL = False
+DISPLAY_WB_MOTION = False # display whole body motion automatically once it's computed
+DT_DISPLAY = 0.05 # dt used to display the wb motion (one configuration every dt is sent to the viewer) It have to be greater than IK_dt
+PLOT = False # Generate plot for various data
+PLOT_CENTROIDAL = False # plot COM trajectory computed by the centroidal dynamic solver, before trying to compute the wholebody motion
+DISPLAY_PLOT = PLOT and True # display plot directly
+SAVE_PLOT = PLOT and True #save plot as svg in OUTPUT_DIR/plot/demo_name_*
+
 ###  Settings for generate_contact_sequence
 FORCE_STRAIGHT_LINE = False # DEBUG ONLY should be false
 
 ### Settings for centroidal script :
-MU=0.5
 GRAVITY = np.matrix([0,0,-9.81]).T
+MU=0.5 # Friction coefficient. hardcoded in timeOpt_configs files, must match this one ! 
 SOLVER_DT = 0.05 # hardcoded in timeOpt_configs files, must match this one ! 
-
-# hardcoded height change between the init (goal) position from planning and the one given to the centroidal solver
+# Hardcoded height change of the COM before the beginning of the motion (value in m and time allowed to make this motion)
+# This is used because for some robot, the reference configuration is really close to the kinematic limits of the robot. 
 COM_SHIFT_Z = 0.0
 TIME_SHIFT_COM = 0.0
-USE_WP_COST = True # use wp from the contact sequence in the cost function of timeopt
+USE_WP_COST = True # use wp from the contact sequence in the cost function of the centroidal solver
 
 ## Settings for end effector :
-EFF_CHECK_COLLISION = True
-WB_ABORT_WHEN_INVALID = False # stop wb script when detecting a collision and return the VALID part (before the phase with collision
-WB_RETURN_INVALID = not WB_ABORT_WHEN_INVALID and True  # stop wb script when detecting a collision and return  the computed part of motion, incuding the last INVALID phase
+EFF_CHECK_COLLISION = True # After generating of whole body motion for a phase with an effector motion, check collision and joints limits for this motion and retry if invalid and if choosen method allow it
+WB_ABORT_WHEN_INVALID = False # stop wb script when stuck with an invalid motion and return the VALID part (before the phase with collision)
+WB_RETURN_INVALID = not WB_ABORT_WHEN_INVALID and True  # stop wb script when stuck with an invalid motion and return  the computed part of motion, incuding the last INVALID phase
 
 ##  Settings for whole body : 
-YAW_ROT_GAIN = 1.
-USE_PLANNING_ROOT_ORIENTATION = True # if true, the reference for the root orientation is the one given by the planning
-# (stored in phase.reference_configurations) if false, use the one of q_init for all the motion.
-WB_VERBOSE = 0 # 0,1 or 2
+YAW_ROT_GAIN = 1. # gain for the orientation task of the root orientation, along the yaw axis (wrt to the other axis of the orientation task)
+USE_PLANNING_ROOT_ORIENTATION = True # if true, the reference for the root orientation is the one given by the planning (stored in phase.reference_configurations) if false, use the one of q_init for all the motion.
+WB_VERBOSE = 0 # 0,1 or 2 Verbosity level for the output of the wholebody script
 WB_STOP_AT_EACH_PHASE = False # wait for user input between each phase
-IK_dt = 0.001  # controler time step
-IK_PRINT_N = 500  # print state of the problem every IK_PRINT_N time steps (if verbose = True)
-CHECK_FINAL_MOTION = True
-IK_store_centroidal = True
+IK_dt = 0.001  # controler time step (in second)
+IK_PRINT_N = 500  # print state of the problem every IK_PRINT_N time steps (if verbose >= 1)
+CHECK_FINAL_MOTION = True # After computation of the motion, check the complete motion for {self-}collision and joints limits
+### The following settings enable the computation of various values stored in the wholeBody_result struct. 
+# Enabling them increase the computation time of the wholeBody script
+IK_store_centroidal = True # c,dc,ddc,L,dL (of the computed wholebody motion)
 IK_store_zmp = True # need store_centroidal
-IK_store_effector = True
+IK_store_effector = True 
 IK_store_contact_forces = True
 
 
