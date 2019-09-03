7d8be20916a6c1bb976c084f6f023a8b21a827af
Pierre Fernbach <pierre.fernbach@laas.fr>
2019-09-03 16:51:16 +0200

    [config] move effector : reduce init phase duration and remove shift_com phase

--- scripts/mlp/demo_configs/talos_moveEffector_flat.py
+++ scripts/mlp/demo_configs/talos_moveEffector_flat.py
@@ -6,11 +6,12 @@ SCRIPT_PATH = "memmo"
 kp_am = 1.
 w_am = 0.5
 
-DURATION_INIT = 1.5 # Time to init the motion
-DURATION_FINAL = 1.5 # Time to stop the robot
+DURATION_INIT = 1. # Time to init the motion
+DURATION_FINAL = 1. # Time to stop the robot
 DURATION_CONNECT_GOAL = 0.
 DURATION_SS = 1.4
-
+COM_SHIFT_Z = 0.
+TIME_SHIFT_COM = 0.
 
 EFF_T_PREDEF = 0.2
 EFF_T_DELAY = 0.05
@@ -18,5 +19,5 @@ FEET_MAX_VEL = 100.
 FEET_MAX_ANG_VEL = 100.
 p_max = 0.07
 
-YAW_ROT_GAIN = 1e-3
+YAW_ROT_GAIN = 1e-6
 end_effector_method = "limbRRToptimized" 
