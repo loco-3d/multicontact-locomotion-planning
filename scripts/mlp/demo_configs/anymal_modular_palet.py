bc12ac09d2a0b69ab629de71c76c97edb0f445d1
pFernbach <pierre.fernbach@gmail.com>
2019-09-15 16:13:48 +0200

    [demo] working config in simu for anymal

--- scripts/mlp/demo_configs/anymal_modular_palet.py
+++ scripts/mlp/demo_configs/anymal_modular_palet.py
@@ -9,23 +9,13 @@ DURATION_FINAL = 2. # Time to stop the robot
 DURATION_FINAL_SS = 1.
 DURATION_SS = 2.
 DURATION_DS = 2.
-DURATION_TS = 0.8
-DURATION_QS = 0.5
+DURATION_TS = 0.5
+DURATION_QS = 0.1
 DURATION_CONNECT_GOAL = 0.
 #COM_SHIFT_Z = -0.02
 #TIME_SHIFT_COM = 1.
-kp_Eff = 100000.                   # proportional gain ofthe effectors motion task
 
-FEET_MAX_VEL = 0.7
-FEET_MAX_ANG_VEL = 1.5
-p_max = 0.25
-EFF_T_PREDEF = 0.
 ## Override default settings :
-YAW_ROT_GAIN = 1.
 #w_rootOrientation = 1.       # weight of the root's orientation task
 #kp_rootOrientation = 5000.     # proportional gain of the root's orientation task
 
-w_posture = 0.01                # weight of joint posture task
-masks_posture = np.matrix(np.zeros(12)).T
-for i in range(4):
-    masks_posture[2+i*3] = 1. # knees
