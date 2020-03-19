a76a72a8d0d0a75344b6b876989a7c75aecabf21
pFernbach <pierre.fernbach@gmail.com>
2020-03-19 15:53:27 +0100

    Add an argument to __main__ to launch it without the viewer

--- python/mlp/__main__.py
+++ python/mlp/__main__.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description="Run a multicontact-locomotion-plan
 parser.add_argument('demo_name', type=str, help="The name of the demo configuration file to load. "
                                                 "Must be a valid python file, either inside the PYTHONPATH"
                                                 "or inside the mlp.demo_configs folder. ")
+parser.add_argument("-n", "--no_viewer", help="Run mlp without visualization.",action="store_true")
 args = parser.parse_args()
 demo_name = args.demo_name
 
@@ -18,26 +19,32 @@ cfg = Config()
 cfg.load_scenario_config(demo_name)
 
 
-# kill already existing instance of the viewer/server
-subprocess.run(["killall", "gepetto-gui"])
+# kill already existing instance of the server
 subprocess.run(["killall", "hpp-rbprm-server"])
-# run the viewer/server in background
+# run the server in background :
 # stdout and stderr outputs of the child process are redirected to devnull (hidden).
 # preexec_fn is used to ignore ctrl-c signal send to the main script (otherwise they are forwarded to the child process)
-process_viewer = subprocess.Popen("gepetto-gui",
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.DEVNULL,
-                                  preexec_fn=os.setpgrp)
 process_server = subprocess.Popen("hpp-rbprm-server",
                                   stdout=subprocess.PIPE,
                                   stderr=subprocess.DEVNULL,
                                   preexec_fn=os.setpgrp)
+# register cleanup methods to kill server when exiting python interpreter
+atexit.register(process_server.kill)
+
+# do the same for the viewer, exept if --no-viewer flag is set
+disable_viewer = args.no_viewer
+print("disable viewer : ", disable_viewer)
+if disable_viewer is None:
+    subprocess.run(["killall", "gepetto-gui"])
+    process_viewer = subprocess.Popen("gepetto-gui",
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.DEVNULL,
+                                      preexec_fn=os.setpgrp)
+    atexit.register(process_viewer.kill)
+
+
 # wait a little for the initialization of the server/viewer
 time.sleep(3)
-
-# register cleanup methods to kill viewer/server when exiting python interpreter
-atexit.register(process_server.kill)
-atexit.register(process_viewer.kill)
 
 
 loco_planner = LocoPlanner(cfg)
