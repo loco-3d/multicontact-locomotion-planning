971a692e09ecb89ce11cb9c90f315015425fc994
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-03-24 17:33:57 +0100

    [Config] update demo talos_obstaclesFeet

--- python/mlp/demo_configs/talos_obstaclesFeet.py
+++ python/mlp/demo_configs/talos_obstaclesFeet.py
@@ -1,14 +1,26 @@
 TIMEOPT_CONFIG_FILE = "cfg_softConstraints_talos.yaml"
 from .common_talos import *
 SCRIPT_PATH = "demos"
+ENV_NAME = "multicontact/floor_bauzil_obstacles"
+DEMO_NAME = "talos_navBauzil_obstacles"
 
-DURATION_INIT = 1.5  # Time to init the motion
-DURATION_FINAL = 1.5  # Time to stop the robot
-DURATION_FINAL_SS = 1.
-DURATION_SS = 2.0
+kp_Eff = 500.  # proportional gain of the effectors motion task
+
+
+DURATION_SS = 1.2
 DURATION_DS = 0.3
-DURATION_TS = 0.4
 
-## Settings for end effectors :
-EFF_T_PREDEF = 0.3
-p_max = 0.1
+kp_rootOrientation = 50000.  # proportional gain of the root's orientation task
+
+EFF_T_PREDEF = 0.2
+EFF_T_DELAY = 0.05
+FEET_MAX_VEL = 1.
+FEET_MAX_ANG_VEL = 1.5
+p_max = 0.07
+USE_PLANNING_ROOT_ORIENTATION = True
+DURATION_CONNECT_GOAL = 0.
+
+GUIDE_STEP_SIZE = 0.6
+
+# Reference config used by the wholeBody script, may be different than the one used by the planning (default value is the same as planning)
+IK_REFERENCE_CONFIG = np.array(Robot.referenceConfig_elbowsUp)
