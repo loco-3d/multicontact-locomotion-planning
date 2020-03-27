1d269eaece320b66a0569e7edbc5f962f0a3f08e
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-03-27 17:07:48 +0100

    [Main] horizon : factorize / clean code

--- python/mlp/loco_planner_horizon.py
+++ python/mlp/loco_planner_horizon.py
@@ -15,6 +15,58 @@ from mlp.utils.cs_tools import computePhasesCOMValues, computeRootTrajFromContac
 
 eigenpy.switchToNumpyArray()
 
+
+def compute_centroidal(generate_centroidal, CentroidalInputs, #CentroidalOutputs,
+                       cfg, fullBody, cs, previous_phase, last_iter = False):
+    # update the initial state with the data from the previous intermediate state:
+    if previous_phase is not None:
+        setInitialFromFinalValues(previous_phase, cs.contactPhases[0])
+
+    if not CentroidalInputs.checkAndFillRequirements(cs, cfg, fullBody):
+        raise RuntimeError(
+            "The current contact sequence cannot be given as input to the centroidal method selected.")
+    cs_full = generate_centroidal(cfg, cs, None, fullBody)
+    #CentroidalOutputs.assertRequirements(cs_full)
+    if last_iter:
+        return cs_full, None
+    else:
+        cs_cut = ContactSequence(0)
+        for i in range(3):
+            cs_cut.append(cs_full.contactPhases[i])
+        return cs_cut, cs_cut.contactPhases[1]
+
+
+def compute_wholebody(generate_effector_trajectories, EffectorInputs, #EffectorOutputs,
+                      generate_wholebody, WholebodyInputs, #WholebodyOutputs,
+                      cfg, fullBody, cs_com,
+                      last_q = None, last_iter = False):
+    ### Effector trajectory reference
+    if not EffectorInputs.checkAndFillRequirements(cs_com, cfg, fullBody):
+        raise RuntimeError(
+            "The current contact sequence cannot be given as input to the end effector method selected.")
+    cs_ref_full = generate_effector_trajectories(cfg, cs_com, fullBody)
+    #EffectorOutputs.assertRequirements(cs_ref_full)
+    if last_iter:
+        cs_ref = cs_ref_full
+    else:
+        cs_cut = ContactSequence()
+        for i in range(2):
+            cs_cut.append(cs_ref_full.contactPhases[i])
+        cs_ref = cs_cut
+
+    if last_q is not None:
+        cs_ref.contactPhases[0].q_init = last_q
+
+    ### Wholebody
+    if not WholebodyInputs.checkAndFillRequirements(cs_ref, cfg, fullBody):
+        raise RuntimeError(
+            "The current contact sequence cannot be given as input to the wholeBody method selected.")
+    cs_wb = generate_wholebody(cfg, cs_ref, fullBody)
+    #WholebodyOutputs.assertRequirements(cs_wb)
+    return cs_wb, cs_wb.contactPhases[-1].q_t(cs_wb.contactPhases[-1].timeFinal)
+
+
+
 class LocoPlannerHorizon(LocoPlanner):
 
 
@@ -44,73 +96,57 @@ class LocoPlannerHorizon(LocoPlanner):
         self.last_wb_phase = None
         self.cs_com_list = [] # FIXME: remove after debug
         super().__init__(cfg)
+        self.generate_centroidal, self.CentroidalInputs, self.CentroidalOutputs = self.cfg.get_centroidal_method()
+        self.generate_effector_trajectories, self.EffectorInputs, self.EffectorOutputs = \
+            self.cfg.get_effector_initguess_method()
+        self.generate_wholebody, self.WholebodyInputs, self.WholebodyOutputs = self.cfg.get_wholebody_method()
 
 
-    def solve_current(self):
+    def solve(self):
 
         self.run_contact_generation()
-        cs_full = self.cs
-        cs_full = computePhasesTimings(cs_full, self.cfg)
-        cs_full = computePhasesCOMValues(cs_full, self.cfg.Robot.DEFAULT_COM_HEIGHT)
-        computeRootTrajFromContacts(self.fullBody, cs_full)
-        setAllUninitializedFrictionCoef(cs_full, self.cfg.MU)
-        self.cs_full = cs_full # FIXME : remove it after debug
+        self.cs
+        self.cs = computePhasesTimings(self.cs, self.cfg)
+        self.cs = computePhasesCOMValues(self.cs, self.cfg.Robot.DEFAULT_COM_HEIGHT)
+        computeRootTrajFromContacts(self.fullBody, self.cs)
+        setAllUninitializedFrictionCoef(self.cs, self.cfg.MU)
 
         pid_centroidal = 0
         first_iter_centroidal = True
         last_iter_centroidal = False
-        print("## Cs size = ", cs_full.size())
-        while pid_centroidal + 5 < cs_full.size():
+        last_q = None
+        last_centroidal_phase = None
+        print("## Cs size = ", self.cs.size())
+        while pid_centroidal + 5 < self.cs.size():
             print("## Current pid = ", pid_centroidal)
-            if pid_centroidal + 7 >= cs_full.size():
+            if pid_centroidal + 7 >= self.cs.size():
                 print("## Last centroidal iter")
                 # last iter, take all the remaining phases
-                num_phase = cs_full.size() - pid_centroidal
+                num_phase = self.cs.size() - pid_centroidal
                 last_iter_centroidal = True
                 cfg.DURATION_CONNECT_GOAL = self.previous_connect_goal
             else:
                 num_phase = 5
             print("## Num phase = ", num_phase)
             # Extract the phases [pid_centroidal; pid_centroidal +num_phases] from cs_full
-            self.cs = ContactSequence(0)
+            cs_iter = ContactSequence(0)
             for i in range(pid_centroidal, pid_centroidal + num_phase):
                 print("-- Add phase : ", i)
-                self.cs.append(cs_full.contactPhases[i])
-            # update the initial state with the data from the previous intermediate state:
-            if not first_iter_centroidal:
-                print("## Set initial from final values")
-                setInitialFromFinalValues(self.cs_com.contactPhases[1], self.cs.contactPhases[0])
+                cs_iter.append(self.cs.contactPhases[i])
 
             # solve the current centroidal problem:
             print("## Run centroidal")
-            self.run_centroidal()
+            cs_com, last_centroidal_phase = compute_centroidal(self.generate_centroidal, self.CentroidalInputs,
+                                        self.cfg, self.fullBody, cs_iter, last_centroidal_phase, last_iter_centroidal)
 
-            print("## Extract first two phases for wholebody")
-            if not last_iter_centroidal:
-                # extract the first 2 phases of cs_com to run the whole body problem:
-                cs_com_full = self.cs_com
-                self.cs_com_list += [cs_com_full]
-                self.cs_com = ContactSequence(0)
-                for i in range(3):
-                    self.cs_com.append(cs_com_full.contactPhases[i])
+            print("## Run wholebody")
+            cs_wb, last_q = compute_wholebody(self.generate_effector_trajectories, self.EffectorInputs,
+                                      self.generate_wholebody, self.WholebodyInputs,
+                                      self.cfg, self.fullBody, cs_com, last_q, last_iter_centroidal)
 
-            self.cs_ref = None
-            self.run_effector_init_guess()
-            if not last_iter_centroidal:
-                cs_ref_full = self.cs_ref
-                self.cs_ref = ContactSequence(0)
-                for i in range(2):
-                    self.cs_ref.append(cs_ref_full.contactPhases[i])
 
-            print("## New cs_ref size : ", self.cs_com.size())
-            if not first_iter_centroidal:
-                self.cs_ref.contactPhases[0].q_init = self.last_wb_phase.q_t(self.last_wb_phase.timeFinal)
-
-            self.run_wholebody()
-            self.last_wb_phase = self.cs_wb.contactPhases[-1]
-            print("## Whole body solved, size : ", self.cs_wb.size())
-            displayWBmotion(self.viewer, self.cs_wb.concatenateQtrajectories(), self.cfg.DT_DISPLAY)
-
+            print("## Whole body solved, size : ", cs_wb.size())
+            displayWBmotion(self.viewer, cs_wb.concatenateQtrajectories(), self.cfg.DT_DISPLAY)
 
             if first_iter_centroidal:
                 first_iter_centroidal = False
@@ -119,7 +155,7 @@ class LocoPlannerHorizon(LocoPlanner):
             pid_centroidal += 2
 
     def run(self):
-        self.solve_current()
+        self.solve()
 
 
 
