5e4a5589bff3cc8697d7a32fae9ddc9524a117d6
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-03-27 16:10:12 +0100

    [Config] flat ground: reduce step duration

--- python/mlp/demo_configs/talos_flatGround.py
+++ python/mlp/demo_configs/talos_flatGround.py
@@ -3,12 +3,13 @@ from .common_talos import *
 SCRIPT_PATH = "demos"
 ENV_NAME = "multicontact/ground"
 
-DURATION_SS = 2.
-DURATION_DS = 0.5
+DURATION_SS = 1.6
+DURATION_DS = 0.4
 
 EFF_T_PREDEF = 0.2
 EFF_T_DELAY = 0.05
 FEET_MAX_VEL = 1.
 FEET_MAX_ANG_VEL = 1.5
 p_max = 0.07
+GUIDE_STEP_SIZE = 0.7
 USE_PLANNING_ROOT_ORIENTATION = False
