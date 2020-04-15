e89624aa12e7a3f2cfc3af8eecc942d0116dade6
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-04-15 13:20:40 +0200

    [Config] update anymal_flatGround

--- python/mlp/demo_configs/anymal_flatGround.py
+++ python/mlp/demo_configs/anymal_flatGround.py
@@ -14,7 +14,8 @@ DURATION_CONNECT_GOAL = 0.
 
 #COM_SHIFT_Z = -0.02
 #TIME_SHIFT_COM = 1.
+#kp_com = 100.                 # proportional gain of center of mass task
+#kp_rootOrientation = 5000.     # proportional gain of the root's orientation task
+YAW_ROT_GAIN = 1.
 
-p_max = 0.18
-
-## Override default settings :
+p_max = 0.25
