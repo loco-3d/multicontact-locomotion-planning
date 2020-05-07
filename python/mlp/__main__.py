1c9bfc09a67f37f50cd0a03ad91c540267f6a5b2
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-05-07 16:23:15 +0200

    [Main] use new ServerManager

--- python/mlp/__main__.py
+++ python/mlp/__main__.py
@@ -4,6 +4,7 @@ import atexit
 import argparse
 import os
 from mlp import LocoPlanner, Config
+from hpp.corbaserver.rbprm.utils import ServerManager
 
 
 # init argument parser
@@ -18,35 +19,14 @@ demo_name = args.demo_name
 cfg = Config()
 cfg.load_scenario_config(demo_name)
 
-
-# kill already existing instance of the server
-subprocess.run(["killall", "hpp-rbprm-server"])
-# run the server in background :
-# stdout and stderr outputs of the child process are redirected to devnull (hidden).
-# preexec_fn is used to ignore ctrl-c signal send to the main script (otherwise they are forwarded to the child process)
-process_server = subprocess.Popen("hpp-rbprm-server",
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.DEVNULL,
-                                  preexec_fn=os.setpgrp)
-# register cleanup methods to kill server when exiting python interpreter
-atexit.register(process_server.kill)
-
-# do the same for the viewer, exept if --no-viewer flag is set
-if not args.no_viewer:
-    subprocess.run(["killall", "gepetto-gui"])
-    process_viewer = subprocess.Popen("gepetto-gui",
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.DEVNULL,
-                                      preexec_fn=os.setpgrp)
-    atexit.register(process_viewer.kill)
-
-
-# wait a little for the initialization of the server/viewer
-time.sleep(3)
-
-
-loco_planner = LocoPlanner(cfg)
-loco_planner.run()
+with ServerManager('hpp-rbprm-server'):
+    if not args.no_viewer:
+        with ServerManager('gepetto-gui'):
+            loco_planner = LocoPlanner(cfg)
+            loco_planner.run()
+    else:
+        loco_planner = LocoPlanner(cfg)
+        loco_planner.run()
 
 
 
