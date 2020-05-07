99cf746198ca75c1b23248cccd9733b21c221c38
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-05-07 16:21:13 +0200

    [Tests] use new ServerManager from rbprm

--- tests/python/test_talos_stairs_sl1m_mopt.py
+++ tests/python/test_talos_stairs_sl1m_mopt.py
@@ -3,14 +3,11 @@
 import unittest
 import subprocess
 import time
+from hpp.corbaserver.rbprm.utils import ServerManager
 from mlp import LocoPlanner, Config
 
 class TestTalosStairsSl1mMopt(unittest.TestCase):
     def test_talos_stairs_sl1m_mopt(self):
-        subprocess.run(["killall", "hpp-rbprm-server"])
-        process = subprocess.Popen("hpp-rbprm-server")
-        time.sleep(3)
-
         cfg = Config()
         cfg.IK_store_centroidal = True
         cfg.IK_store_zmp = True
@@ -24,26 +21,24 @@ class TestTalosStairsSl1mMopt(unittest.TestCase):
         cfg.wholebody_method="none"
         cfg.ITER_DYNAMIC_FILTER = 0
 
-        loco_planner = LocoPlanner(cfg)
-        loco_planner.run()
+        with ServerManager('hpp-rbprm-server'):
+            loco_planner = LocoPlanner(cfg)
+            loco_planner.run()
 
-        self.assertTrue(loco_planner.cs.size() > 2)
-        self.assertTrue(loco_planner.cs.size() < 50)
-        self.assertEqual(loco_planner.cs.size(), loco_planner.cs_com.size())
-        self.assertEqual(loco_planner.cs.size(), loco_planner.cs_ref.size())
-        self.assertIsNone(loco_planner.cs_wb)
+            self.assertTrue(loco_planner.cs.size() > 2)
+            self.assertTrue(loco_planner.cs.size() < 50)
+            self.assertEqual(loco_planner.cs.size(), loco_planner.cs_com.size())
+            self.assertEqual(loco_planner.cs.size(), loco_planner.cs_ref.size())
+            self.assertIsNone(loco_planner.cs_wb)
 
-        # check that the sequence contains the expected data:
-        self.assertTrue(loco_planner.cs.haveConsistentContacts())
+            # check that the sequence contains the expected data:
+            self.assertTrue(loco_planner.cs.haveConsistentContacts())
 
-        self.assertTrue(loco_planner.cs_ref.haveConsistentContacts())
-        self.assertTrue(loco_planner.cs_ref.haveTimings())
-        self.assertTrue(loco_planner.cs_ref.haveCentroidalTrajectories())
-        self.assertTrue(loco_planner.cs_ref.haveZMPtrajectories())
-        self.assertTrue(loco_planner.cs_ref.haveEffectorsTrajectories(1e-2))
-
-        process.kill()
-
+            self.assertTrue(loco_planner.cs_ref.haveConsistentContacts())
+            self.assertTrue(loco_planner.cs_ref.haveTimings())
+            self.assertTrue(loco_planner.cs_ref.haveCentroidalTrajectories())
+            self.assertTrue(loco_planner.cs_ref.haveZMPtrajectories())
+            self.assertTrue(loco_planner.cs_ref.haveEffectorsTrajectories(1e-2))
 
 
 if __name__ == '__main__':
