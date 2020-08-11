54207e752135beee26e47f1d95083ae0e4e57092
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-08-11 14:14:18 +0200

    [Demo] solo flat ground: use faster gait

--- python/mlp/contact_sequence/manually_defined/solo_walk.py
+++ python/mlp/contact_sequence/manually_defined/solo_walk.py
@@ -18,10 +18,9 @@ cs = ContactSequence(0)
 limbs = fb.limbs_names
 #Create an initial contact phase :
 q_ref = fb.referenceConfig[::] + [0] * 6
-addPhaseFromConfig(fb, cs, q_ref, limbs, t_init=0)
-cs.contactPhases[-1].time_final = 1.
+addPhaseFromConfig(fb, cs, q_ref, limbs)
 
-walk(fb, cs, 1., 0.07, limbs, duration_ss=0.5, duration_ds=0.2)
+walk(fb, cs, 1., 0.12, limbs)
 
 display_tools.displaySteppingStones(cs, gui, sceneName, fb)
 
