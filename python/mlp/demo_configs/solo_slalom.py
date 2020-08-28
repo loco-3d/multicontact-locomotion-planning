164ec1b2978ba18f26b5a897f05ca51fe63e7af4
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-08-28 15:34:01 +0200

    [Demo] add/update demos for solo: flat ground, pallet, slalom

--- python/mlp/demo_configs/solo_slalom.py
+++ python/mlp/demo_configs/solo_slalom.py
@@ -1,14 +1,15 @@
 TIMEOPT_CONFIG_FILE = "cfg_softConstraints_solo.yaml"
 from .common_solo import *
 SCRIPT_PATH = "demos"
-ENV_NAME = "multicontact/ground"
+ENV_NAME = "multicontact/slalom_debris"
 
 # phases duration
 DURATION_INIT = 1.  # Time to init the motion
 DURATION_FINAL = 1.  # Time to stop the robot
-DURATION_TS = 0.4
+DURATION_TS = 0.5
 DURATION_QS = 0.1
 DURATION_CONNECT_GOAL = 2.
 USE_PLANNING_ROOT_ORIENTATION = True
 GUIDE_MAX_YAW = 0.5 # maximal yaw rotation difference between two discretization step
+GUIDE_STEP_SIZE = 0.5
 
