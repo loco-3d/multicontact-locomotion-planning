94e101c02e9c8d3fd5c2a451559f98dd03fcd3cd
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-08-28 15:33:27 +0200

    [Config] common_solo: update wb cost function to use solo8

--- python/mlp/demo_configs/common_solo.py
+++ python/mlp/demo_configs/common_solo.py
@@ -14,7 +14,7 @@ fMax = 25.  # maximum normal force
 w_com = 1.0  # weight of center of mass task
 w_am =  2e-2
 w_am_track = 2e-2 # weight used for the tracking of the Angular momentum
-w_posture = 0.01  # weight of joint posture task
+w_posture = 10.  # weight of joint posture task
 w_rootOrientation = 0.1  # weight of the root's orientation task
 w_eff = 1.0  # weight of the effector motion task
 kp_contact = 50.0  # proportional gain of contact constraint
@@ -22,8 +22,8 @@ kp_com = 30.  # proportional gain of center of mass task
 kp_am = 20.
 kp_am_track = 20. # gain used for the tracking of the Angular momentum
 kp_posture = 10.  # proportional gain of joint posture task
-kp_rootOrientation = 5000.  # proportional gain of the root's orientation task
-kp_Eff = 100.  # proportional gain of the effectors motion task
+kp_rootOrientation = 50.  # proportional gain of the root's orientation task
+kp_Eff = 10.  # proportional gain of the effectors motion task
 level_eff = 0
 level_com = 0
 level_posture = 1
@@ -39,7 +39,7 @@ scaling_vel_bounds = 1.
 # The weight of the force regularization task of each contact will start at w_forceRef_init when creating a new contact,
 # and then linearly reduce to w_forceRef_end over a time period of phase_duration * w_forceRef_time_ratio
 w_forceRef_init = 1.
-w_forceRef_end = 1e-5
+w_forceRef_end = 1e-3
 w_forceRef_time_ratio = 0.5
 
 YAW_ROT_GAIN = 1.
@@ -65,15 +65,24 @@ FEET_MAX_ANG_VEL = 1.5
 p_max = 0.05
 EFF_T_PREDEF = 0.
 
+
 GUIDE_STEP_SIZE = 0.5
 SL1M_USE_MIP = True
-SL1M_USE_INTERSECTION = True
-SL1M_MAX_STEP = 10
+SL1M_USE_INTERSECTION = False
+SL1M_MAX_STEP = 6
 
 import numpy as np
 gain_vector = np.ones(12)
 masks_posture = np.zeros(12)
-for i in range(4):
-    masks_posture[2 + i * 3] = 1.  # knees
+
 
 IK_REFERENCE_CONFIG = np.array(Robot.referenceConfig)
+
+# for solo 8 : set a velocity bound to 0 for shoulder roll joint:
+scaling_vel_bounds = np.ones(12)
+
+for i in range(4):
+    masks_posture[i * 3] = 1.  # shoulder roll
+    scaling_vel_bounds[i * 3] = 0.
+    gain_vector[i * 3] = 1000.
+
