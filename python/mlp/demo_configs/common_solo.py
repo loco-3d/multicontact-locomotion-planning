ca743b4fe83935fe1a8cb7268cef3f2235f41576
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-09-15 11:26:24 +0200

    [Config] solo: put all task at level 1 and adapt torque constraint task

--- python/mlp/demo_configs/common_solo.py
+++ python/mlp/demo_configs/common_solo.py
@@ -12,27 +12,27 @@ IK_dt = 0.002
 fMin = 1.0  # minimum normal force
 fMax = 25.  # maximum normal force
 w_com = 1.0  # weight of center of mass task
-w_am =  2e-2
-w_am_track = 2e-2 # weight used for the tracking of the Angular momentum
-w_posture = 10.  # weight of joint posture task
+w_am =  0.
+w_am_track = 0. # weight used for the tracking of the Angular momentum
+w_posture = 0.1  # weight of joint posture task
 w_rootOrientation = 0.1  # weight of the root's orientation task
 w_eff = 1.0  # weight of the effector motion task
 kp_contact = 50.0  # proportional gain of contact constraint
-kp_com = 30.  # proportional gain of center of mass task
+kp_com = 100.  # proportional gain of center of mass task
 kp_am = 20.
 kp_am_track = 20. # gain used for the tracking of the Angular momentum
 kp_posture = 10.  # proportional gain of joint posture task
 kp_rootOrientation = 50.  # proportional gain of the root's orientation task
 kp_Eff = 10.  # proportional gain of the effectors motion task
-level_eff = 0
-level_com = 0
+level_eff = 1
+level_com = 1
 level_posture = 1
-level_rootOrientation = 0
+level_rootOrientation = 1
 level_am = 1
 
 # scaling and weight for the bounds tasks in TSID:
-w_torque_bounds = 1000.
-w_joint_bounds = 1000.
+w_torque_bounds = 1.
+w_joint_bounds = 0.
 scaling_torque_bounds = 1.5
 scaling_vel_bounds = 1.
 
@@ -46,7 +46,7 @@ YAW_ROT_GAIN = 1.
 
 IK_eff_size = Robot.dict_size.copy()
 PLOT_CIRCLE_RADIUS = 0.01 # radius of the circle used to display the contacts
-SOLVER_DT = 0.05  # time step used for centroidal methods
+SOLVER_DT = 0.01  # time step used for centroidal methods
 
 # phases duration
 DURATION_INIT = 1.  # Time to init the motion
@@ -84,5 +84,5 @@ scaling_vel_bounds = np.ones(12)
 for i in range(4):
     masks_posture[i * 3] = 1.  # shoulder roll
     scaling_vel_bounds[i * 3] = 0.
-    gain_vector[i * 3] = 1000.
+    #gain_vector[i * 3] = 1.
 
