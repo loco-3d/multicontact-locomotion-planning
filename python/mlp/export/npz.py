2c0a9229ed727a616f4516dc5ed5ecf9e16be0f5
Pierre Fernbach <pierre.fernbach@gmail.com>
2022-04-11 18:39:54 +0200

    Export npz in /res without subfolder

--- python/mlp/export/npz.py
+++ python/mlp/export/npz.py
@@ -4,7 +4,7 @@ from mlp.utils.requirements import Requirements
 
 def export(cfg, cs_ref, cs):
     dt = cfg.IK_dt
-    path = cfg.EXPORT_PATH + "/npz"
+    path = cfg.EXPORT_PATH
     name = cfg.DEMO_NAME + ".npz"
     Requirements.requireRootTrajectories(cs_ref, cfg)
     Requirements.requireZMPtrajectories(cs_ref, cfg)
